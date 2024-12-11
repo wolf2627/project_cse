0e0abe94c3a75e58546f1fac8819c2449eaf63cc
wolf <aswfrln@gmail.com>
2024-12-11 06:42:29 +0000

    sidebar icons modification

--- htdocs/__templates/_sidebar.php
+++ htdocs/__templates/_sidebar.php
@@ -22,31 +22,31 @@ $role = Session::get('role');
 
             <ul class="nav flex-column mb-auto">
 
-            <?php if ($role == "admin"): ?>
-                <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
-                    <span>Reports</span>
-                    <a class="link-secondary" href="#" aria-label="Add a new report">
-                    </a>
-                </h6>
-
-                <?
-
-                // Fetch tests from the database
-                $tests = Test::getTests();
-                ?>
-
-                <?php foreach ($tests as $test): ?>
-                    <li class="nav-item">
-                        <a class="nav-link d-flex align-items-center gap-2" href="/report?testid=<?= base64_encode((string)$test->_id) ?>&testname=<?= base64_encode($test->testname) ?>&dept=<?= base64_encode($test->department) ?>">
-                            <svg class="bi">
-                                <use xlink:href="#file-earmark-text" />
-                            </svg>
-                            <?= htmlspecialchars($test['testname']) ?>
-                            <span class="text-muted ms-2">(<?= $test['status'] === 'active' ? 'Available' : 'Coming Soon' ?>)</span>
+                <?php if ($role == "admin"): ?>
+                    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
+                        <span>Reports</span>
+                        <a class="link-secondary" href="#" aria-label="Add a new report">
                         </a>
-                    </li>
-                <?php endforeach; ?>
-                <?php endif;?>
+                    </h6>
+
+                    <?
+
+                    // Fetch tests from the database
+                    $tests = Test::getTests();
+                    ?>
+
+                    <?php foreach ($tests as $test): ?>
+                        <li class="nav-item">
+                            <a class="nav-link d-flex align-items-center gap-2" href="/report?testid=<?= base64_encode((string)$test->_id) ?>&testname=<?= base64_encode($test->testname) ?>&dept=<?= base64_encode($test->department) ?>">
+                                <svg class="bi">
+                                    <use xlink:href="#file-earmark-text" />
+                                </svg>
+                                <?= htmlspecialchars($test['testname']) ?>
+                                <span class="text-muted ms-2">(<?= $test['status'] === 'active' ? 'Available' : 'Coming Soon' ?>)</span>
+                            </a>
+                        </li>
+                    <?php endforeach; ?>
+                <?php endif; ?>
 
                 <?php if ($role == "faculty"): ?>
                     <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
@@ -162,7 +162,7 @@ $role = Session::get('role');
                     <li class="nav-item">
                         <a class="nav-link d-flex align-items-center gap-2" href="/enrollsubjects">
                             <svg class="bi">
-                                <use xlink:href="#file-earmark-text" />
+                                <use xlink:href="#file-person-fill" />
                             </svg>
                             Enroll Students
                         </a>
@@ -171,37 +171,67 @@ $role = Session::get('role');
                     <li class="nav-item">
                         <a class="nav-link d-flex align-items-center gap-2" href="/assignfaculty">
                             <svg class="bi">
-                                <use xlink:href="#file-earmark-text" />
+                                <use xlink:href="#bi-person-rolodex" />
                             </svg>
                             Assign Faculty
                         </a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link d-flex align-items-center gap-2" href="/assignrole">
+                        <button class="nav-link d-flex align-items-center gap-2 btn-toggle" data-bs-toggle="collapse" data-bs-target="#Role-collapse" aria-expanded="false">
                             <svg class="bi">
-                                <use xlink:href="#file-earmark-text" />
+                                <use xlink:href="#new-folder"></use>
                             </svg>
-                            Assign Role
-                        </a>
-                    </li>
+                            Role
+                            <svg class="bi arrow-mov ">
+                                <use xlink:href="#plus"></use>
+                            </svg>
+                        </button>
+                        <div class="collapse" id="Role-collapse">
+                            <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                <li class="nav-item">
+                                    <a class="nav-link d-flex align-items-center gap-2" href="/assignrole">
+                                        <svg class="bi">
+                                            <use xlink:href="#file-earmark-text" />
+                                        </svg>
+                                        Assign
+                                    </a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link d-flex align-items-center gap-2" href="/managerole">
+                                        <svg class="bi">
+                                            <use xlink:href="#file-earmark-text" />
+                                        </svg>
+                                        Manage
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+
 
                     <li class="nav-item">
-                        <a class="nav-link d-flex align-items-center gap-2" href="/managerole">
+                        <button class="nav-link d-flex align-items-center gap-2 btn-toggle" data-bs-toggle="collapse" data-bs-target="#Permission-collapse" aria-expanded="false">
                             <svg class="bi">
-                                <use xlink:href="#file-earmark-text" />
+                                <use xlink:href="#new-folder"></use>
                             </svg>
-                            Manage Role
-                        </a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link d-flex align-items-center gap-2" href="/managepermission">
-                            <svg class="bi">
-                                <use xlink:href="#file-earmark-text" />
+                            Permission
+                            <svg class="bi arrow-mov ">
+                                <use xlink:href="#plus"></use>
                             </svg>
-                            Manage Permission
-                        </a>
+                        </button>
+                        <div class="collapse" id="Permission-collapse">
+                            <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                <li class="nav-item">
+                                    <a class="nav-link d-flex align-items-center gap-2" href="/managepermission">
+                                        <svg class="bi">
+                                            <use xlink:href="#file-earmark-text" />
+                                        </svg>
+                                        Manage
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </li>
 
                 <?php endif; ?>
