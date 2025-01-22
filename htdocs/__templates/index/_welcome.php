074b5abd85ae5f5f7a45ed5d01fff55930fbe6dc
yuheswari v <yuheswari2525@gmail.com>
2025-01-22 07:29:35 +0000

    ui sidebar enhance

--- htdocs/__templates/index/_welcome.php
+++ htdocs/__templates/index/_welcome.php
@@ -2,7 +2,7 @@
         <div class="text-center">
             <img class="mb-4 img-fluid" src="https://www.psnacet.edu.in/images/logo-white.png" alt="Portal Logo" width="" height="">
             <h1 class="h3 fw-bold text-warning">Welcome to the Portal</h1>
-            <p class="text-muted mb-4">Effortless management starts here.</p>
+            <p class="mb-4 text-white">Effortless management starts here.</p>
             <a href="/login" class="btn btn-success w-50 py-2">Login</a>
         </div>
 </section>
