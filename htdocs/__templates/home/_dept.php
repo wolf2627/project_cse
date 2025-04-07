3d7672555dffeaabfbbf751b487bfbd3cced1178
wolf <aswfrln@gmail.com>
2025-04-07 00:00:11 +0000

    temp home

--- htdocs/__templates/home/_dept.php
+++ htdocs/__templates/home/_dept.php
@@ -1,14 +1,18 @@
 <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active" data-bs-interval="10000">
-      <img src="required/home/main.jpeg" class="d-block w-100" alt="...">
+      <img src="required/uploads/home/main1.jpg" class="d-block w-100" style="height: 900px; width: auto;" alt="...">
     </div>
     <div class="carousel-item" data-bs-interval="2000">
-      <img src="required/home/main.jpeg" class="d-block w-100" alt="...">
+      <img src="required/uploads/home/main2.jpg" class="d-block w-100" style="height: 900px; width: auto;" alt="...">
     </div>
-    <!-- <div class="carousel-item">
-      <img src="requried/home/main.jpeg" class="d-block w-100" alt="...">
-    </div> -->
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="required/uploads/home/main3.jpg" class="d-block w-100" style="height: 900px; width: auto;" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="required/uploads/home/main4.jpg" class="d-block w-100" style="height: 900px; width: auto;" alt="...">
+    </div>
+   
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
